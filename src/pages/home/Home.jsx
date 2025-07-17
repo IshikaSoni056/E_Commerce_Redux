dc841fee9bc581ae64c5bbed366b01012ff88f5f
IshikaSoni056 <115107354+IshikaSoni056@users.noreply.github.com>
2025-07-18 03:39:18 +0530

    product clearing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,9 @@
-import {  Offer, Product,  } from "../../utils/Route";
+import {   Product,  } from "../../utils/Route";
 
 export const Home = () => {
   return (
-    <>
-      {/* <Hero /> */}
-      <Offer />
-      {/* <Category /> */}
-      <Product />
-      {/* <Support /> */}
+    <> 
+     <Product />   
     </>
   );
 };
--- src/screen/hero/Offer.jsx
+++ src/screen/hero/Offer.jsx
@@ -69,61 +69,6 @@ export const Offer = () => {
     </>
   );
 };
-// export const FlaseSales = () => {
-//   const settings = {
-//     dots: false,
-//     infinite: true,
-//     slidesToShow: 1,
-//     speed: 500,
-//     nextArrow: <SampleNextArrow />,
-//     prevArrow: <SamplePrevArrow />,
-//   };
-//   return (
-//     <div className="flaseSale">
-//       <div className="content">
-//         <div className="imageWrapper">
-//           <img src="../images/offer/banner-2.webp" alt="banner" />
-//           <div className="animatedBg"></div>
-//         </div>
-//         <div className="products">
-//           <h1>Flash Sale</h1>
-//           <Slider {...settings}>
-//             {flaseproduct.map((product) => {
-//               const available = product.qty - product.sold;
-//               const soldPercentage = (product.sold / product.qty) * 100;
-//               return (
-//                 <>
-//                   <div className="product" key={product.id}>
-//                     <div className="img">
-//                       <img src={product.cover} alt="cover" />
-//                     </div>
-//                     <div className="details">
-//                       <h3>{product.name}</h3>
-//                       <p>{product.desc.slice(0, 35)}...</p>
-//                       <div className="price">
-//                         <h3 className="price-tag">${product.dprice}.00</h3>
-//                         <h3 className="underline">${product.price}.00</h3>
-//                       </div>
-//                     </div>
-//                   </div>
-//                   <div className="progress">
-//                     <div className="flex">
-//                       <p>Sold : {product.sold}</p>
-//                       <p>Available : {available}</p>
-//                     </div>
-//                     <div className="progressBar">
-//                       <div className="progressFill" style={{ width: `${soldPercentage}%` }}></div>
-//                     </div>
-//                   </div>
-//                 </>
-//               );
-//             })}
-//           </Slider>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
 export const FlaseSales = () => {
   return (
